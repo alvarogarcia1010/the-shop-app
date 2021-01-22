9fb40416e36d6b145bb0bd3fdccb3460d21350d5
alvarogarcia1010 <00111317@uca.edu.sv>
2021-01-21 21:48:38 -0600

    Issue #1

--- component/shop/ProductItem.js
+++ component/shop/ProductItem.js
@@ -1,22 +1,34 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet, Button } from 'react-native'
+import { View, Text, Image, StyleSheet, Button, Platform } from 'react-native'
+import { TouchableNativeFeedback, TouchableOpacity } from 'react-native-gesture-handler'
 import Colors from '../../constants/Colors'
 
 const ProductItem = props => {
+  let TouchableCmp = TouchableOpacity
+
+  if(Platform.OS === 'android' && Platform.Version >= 21)
+  {
+    TouchableCmp = TouchableNativeFeedback
+  }
+  
   return (
     <View style={styles.product}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{uri: props.image}} />
-      </View>
-      
-      <View style={styles.details}>
-        <Text style={styles.title}>{props.title}</Text>
-        <Text style={styles.price}>$ {props.price.toFixed(2)}</Text>
-      </View>
-      
-      <View style={styles.actions}>
-        <Button color={Colors.primary} title="View details" onPress={props.onViewDetail}/>
-        <Button color={Colors.primary} title="To card" onPress={props.onAddToCart}/>
+      <View style={styles.touchable}>
+        <TouchableCmp onPress={props.onViewDetail} useForeground>
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={{uri: props.image}} />
+          </View>
+          
+          <View style={styles.details}>
+            <Text style={styles.title}>{props.title}</Text>
+            <Text style={styles.price}>$ {props.price.toFixed(2)}</Text>
+          </View>
+          
+          <View style={styles.actions}>
+            <Button color={Colors.primary} title="View details" onPress={props.onViewDetail}/>
+            <Button color={Colors.primary} title="To card" onPress={props.onAddToCart}/>
+          </View>
+        </TouchableCmp>
       </View>
     </View>
   )
@@ -32,7 +44,12 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: 'white',
     height: 300,
-    margin: 20
+    margin: 20,
+    
+  },
+  touchable: {
+    borderRadius: 10,
+    overflow: 'hidden'
   },
   imageContainer: {
     width: '100%',
--- screen/shop/ProductDetailScreen.js
+++ screen/shop/ProductDetailScreen.js
@@ -1,21 +1,55 @@
-import React from 'react'
-import { View, Text, Image, Button, StyleSheet } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
+import React from "react";
+import { View, Text, Image, Button, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { useSelector } from "react-redux";
+import Colors from "../../constants/Colors";
 
-const ProductDetailScreen = () => {
+const ProductDetailScreen = (props) => {
+  const productId = props.navigation.getParam("productId");
+  const selectedProduct = useSelector((state) =>
+    state.products.availableProducts.find((prod) => prod.id === productId)
+  );
+
   return (
     <ScrollView>
       <View>
-      
+        <Image style={styles.image} source={{uri: selectedProduct.imageUrl}} />
+        <View style={styles.actions}>
+          <Button color={Colors.primary} title="Add to cart" onPress={() => {}} />
+        </View>
+        <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+        <Text style={styles.description}>{selectedProduct.description}</Text>
       </View>
     </ScrollView>
-
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  
-})
+  image: {
+    width: '100%',
+    height: 300
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center'
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20
+  }
+});
 
+ProductDetailScreen.navigationOptions = navData => {
+  return {
+    headerTitle: navData.navigation.getParam('productTitle')
+  }
+}
 
-export default ProductDetailScreen
+export default ProductDetailScreen;
