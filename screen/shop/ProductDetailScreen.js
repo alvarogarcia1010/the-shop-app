e29be499f410711a625d9b10080a27df29759b7a
alvarogarcia1010 <00111317@uca.edu.sv>
2021-01-24 21:39:35 -0600

    Issue #1

--- screen/shop/ProductDetailScreen.js
+++ screen/shop/ProductDetailScreen.js
@@ -7,7 +7,7 @@ import Colors from "../../constants/Colors";
 
 const ProductDetailScreen = (props) => {
   const productId = props.navigation.getParam("productId");
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const selectedProduct = useSelector((state) =>
     state.products.availableProducts.find((prod) => prod.id === productId)
   );
@@ -15,9 +15,16 @@ const ProductDetailScreen = (props) => {
   return (
     <ScrollView>
       <View>
-        <Image style={styles.image} source={{uri: selectedProduct.imageUrl}} />
+        <Image
+          style={styles.image}
+          source={{ uri: selectedProduct.imageUrl }}
+        />
         <View style={styles.actions}>
-          <Button color={Colors.primary} title="Add to cart" onPress={() => dispatch(cartActions.addToCart(selectedProduct))} />
+          <Button
+            color={Colors.primary}
+            title="Add to cart"
+            onPress={() => dispatch(cartActions.addToCart(selectedProduct))}
+          />
         </View>
         <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
         <Text style={styles.description}>{selectedProduct.description}</Text>
@@ -28,32 +35,32 @@ const ProductDetailScreen = (props) => {
 
 const styles = StyleSheet.create({
   image: {
-    width: '100%',
-    height: 300
+    width: "100%",
+    height: 300,
   },
   actions: {
     marginVertical: 10,
-    alignItems: 'center'
+    alignItems: "center",
   },
   price: {
     fontSize: 20,
-    color: '#888',
-    textAlign: 'center',
+    color: "#888",
+    textAlign: "center",
     marginVertical: 20,
-    fontFamily: 'open-sans-bold'
+    fontFamily: "open-sans-bold",
   },
   description: {
     fontSize: 14,
-    textAlign: 'center',
+    textAlign: "center",
     marginHorizontal: 20,
-    fontFamily: 'open-sans'
-  }
+    fontFamily: "open-sans",
+  },
 });
 
-ProductDetailScreen.navigationOptions = navData => {
+ProductDetailScreen.navigationOptions = (navData) => {
   return {
-    headerTitle: navData.navigation.getParam('productTitle')
-  }
-}
+    headerTitle: navData.navigation.getParam("productTitle"),
+  };
+};
 
 export default ProductDetailScreen;
